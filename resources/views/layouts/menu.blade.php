1646e3e2411d2e16c703e355b81c47e3842f9fe6
Renato Gomes <renatoo.agomes@gmail.com>
2017-07-12 03:38:31 -0300

    Commit removendo resources desnecesarias (views) geradas pelo scaffold do laravelgenerator

--- resources/views/layouts/menu.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- <li class="{{ Request::is('cotacaoPacotes*') ? 'active' : '' }}">
-    <a href="{!! route('cotacaoPacotes.index') !!}"><i class="fa fa-edit"></i><span>CotacaoPacotes</span></a>
-</li> --}}
-
