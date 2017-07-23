406f5d89d1566007b960874d4888569c85e11bd8
Leandro Acquati <leandroacquati@gmail.com>
2017-07-23 00:26:51 -0300

    /home Alterando a imagem e o nome do usuário, retirada da opção perfil e search.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,12 +34,56 @@
             <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
                 <span class="sr-only">Toggle navigation</span>
             </a>
+
+            <!-- Navbar Right Menu -->
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <!-- User Account Menu -->
+                    <li class="dropdown user user-menu">
+                        <!-- Menu Toggle Button -->
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <!-- The user image in the navbar-->
+                            <img src="{{ asset('icones/user-vivala.png') }}"
+                                 class="user-image" alt="User Image"/>
+                            <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                            <span class="hidden-xs">{!! Auth::user()->name !!}</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- The user image in the menu -->
+                            <li class="user-header">
+                                <img src="{{ asset('icones/user-vivala.png') }}"
+                                     class="img-circle" alt="User Image"/>
+                                <p>
+                                    {!! Auth::user()->name !!}
+                                    <small>Membro desde {!! Auth::user()->created_at->format('M. Y') !!}</small>
+                                </p>
+                            </li>
+                            <!-- Menu Footer-->
+                            <li class="user-footer">
+                                <!--div class="pull-left">
+                                    <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                                </div-->
+                                <div class="pull-right">
+                                    <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat"
+                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Sign out
+                                    </a>
+                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
+                                          style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 
     <!-- Left side column. contains the logo and sidebar -->
-@include('layouts.sidebar')
-<!-- Content Wrapper. Contains page content -->
+    @include('layouts.sidebar')
+    <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         @yield('content')
     </div>
