648b64906dba95bba450dacd55b5cec2cb5f4664
Renato Gomes <renatoo.agomes@gmail.com>
2017-07-24 16:09:44 -0300

    + ajustes em pacotes

--- app/Http/Requests/API/CreateCotacaoPacoteAPIRequest.php
+++ app/Http/Requests/API/CreateCotacaoPacoteAPIRequest.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Requests\API;
 
+use App\Http\Requests\API\BaseAPIRequest;
 use App\Models\CotacaoPacote;
-use InfyOm\Generator\Request\APIRequest;
 
-class CreateCotacaoPacoteAPIRequest extends APIRequest
+class CreateCotacaoPacoteAPIRequest extends BaseAPIRequest
 {
     /**
      * Determine if the user is authorized to make this request.
