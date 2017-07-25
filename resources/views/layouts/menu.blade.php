a5673052d6ab1185f73b12ad00220dcefd298ff4
Leandro Acquati <leandroacquati@gmail.com>
2017-07-25 00:56:30 -0300

    alterando a sidebar /home

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,9 +1,9 @@
 <li class="{{ Request::is('inscricaoNewsletters*') ? 'active' : '' }}">
-    <a href="{!! route('inscricaoNewsletters.index') !!}"><i class="fa fa-edit"></i><span>InscricaoNewsletters</span></a>
+    <a href="{!! route('inscricaoNewsletters.index') !!}"><i class="fa fa-envelope-o" aria-hidden="true"></i><span>Inscrições Newsletter</span></a>
 </li>
 
 <li class="{{ Request::is('experiencias*') ? 'active' : '' }}">
-    <a href="{!! route('experiencias.index') !!}"><i class="fa fa-edit"></i><span>Experiencias</span></a>
+    <a href="{!! route('experiencias.index') !!}"><i class="fa fa-heartbeat" aria-hidden="true"></i><span>Experiências</span></a>
 </li>
 
 {{-- ROTA DESATIVADA, VAI SER ACESSADA DE DENTRO DA EXP
@@ -18,10 +18,8 @@
 </li>
 --}}
 
-
-
 <li class="{{ Request::is('expedicaos*') ? 'active' : '' }}">
-    <a href="{!! route('expedicaos.index') !!}"><i class="fa fa-edit"></i><span>Expedicaos</span></a>
+    <a href="{!! route('expedicaos.index') !!}"><i class="fa fa-safari" aria-hidden="true"></i></i><span>Expedições</span></a>
 </li>
 
 {{--  ROTA DESATIVADA, VAI SER ACESSADA DE DENTRO DA EXP
@@ -36,17 +34,20 @@
 </li>
 --}}
 
-<li class="{{ Request::is('cotacaoHospedagems*') ? 'active' : '' }}">
-    <a href="{!! route('cotacaoHospedagems.index') !!}"><i class="fa fa-edit"></i><span>CotacaoHospedagems</span></a>
+<!-- Dropdown Menu Cotações -->
+<li class="treeview" id="scrollspy-components">
+  <a href="javascript:void(0)"><i class="fa fa-circle-o"></i>Cotações</a>
+  <ul class="nav treeview-menu">
+    <li class="{{ Request::is('cotacaoHospedagems*') ? 'active' : '' }}">
+        <a href="{!! route('cotacaoHospedagems.index') !!}"><i class="fa fa-bed" aria-hidden="true"></i><span>Hospedagens</span></a>
+    </li>
+
+    <li class="{{ Request::is('cotacaoPacotes*') ? 'active' : '' }}">
+        <a href="{!! route('cotacaoPacotes.index') !!}"><i class="fa fa-suitcase" aria-hidden="true"></i></i><span>Pacotes</span></a>
+    </li>
+
+    <li class="{{ Request::is('cotacaoAereos*') ? 'active' : '' }}">
+        <a href="{!! route('cotacaoAereos.index') !!}"><i class="fa fa-plane" aria-hidden="true"></i></i><span>Aéreos</span></a>
+    </li>
+  </ul>
 </li>
-
-
-
-<li class="{{ Request::is('cotacaoPacotes*') ? 'active' : '' }}">
-    <a href="{!! route('cotacaoPacotes.index') !!}"><i class="fa fa-edit"></i><span>CotacaoPacotes</span></a>
-</li>
-
-<li class="{{ Request::is('cotacaoAereos*') ? 'active' : '' }}">
-    <a href="{!! route('cotacaoAereos.index') !!}"><i class="fa fa-edit"></i><span>CotacaoAereos</span></a>
-</li>
-
