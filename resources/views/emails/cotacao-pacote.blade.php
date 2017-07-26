250af1f42b36b18b4b08037a0776714487b17c97
Leandro Acquati <leandroacquati@gmail.com>
2017-07-26 05:18:30 -0300

    Terminando o email de pedido de cotação de pacote completo /exemplo-email-cotacao-pacote

--- resources/views/emails/cotacao-pacote.blade.php
+++ resources/views/emails/cotacao-pacote.blade.php
@@ -10,58 +10,114 @@
         <tbody>
             <tr align="center">
                 <td align="center" style="text-align:center;">
-                    <h3>Dados do cliente:</h3>
-<p style="font-size:14px; font-weight:normal; margin-top:10px;">
+                    <h3 style="font-family:'Titillium Web', sans-serif;">Dados do cliente:</h3>
 
-<span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Nome completo: </span> {{ $cotacao->nome_completo }} <br>
-<span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Prefere ser chamado de: </span> {{ $cotacao->nome_preferencia }} <br>
-<span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Email: </span> {{ $cotacao->email }} <br>
-<span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Telefone: </span> {{ $cotacao->telefone }} <br>
-<span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Contato enviado em: </span> {{ $cotacao->created_at->format('d/m/Y - H:i') }} <br>
-</p>
+                    <p style="font-size:18px; font-weight:normal; margin-top:10px; text-align: left;">
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Nome completo: </span>
+                        {{ $cotacao->nome_completo }}<br>
 
-<span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">origem</span> {{ $cotacao->origem }} <br>
-'destino',
-'data_ida',
-'data_volta',
-'datas_flexiveis',
-'qnt_adultos',
-'qnt_criancas',
-'qnt_bebes',
-'periodo_voo_ida',
-'periodo_voo_volta',
-'aeroporto_origem',
-'aeroporto_retorno',
-'companias_aereas_preferenciais',
-'numero_paradas',
-'tempo_voo',
-'aereo_preco_desejado',
-'hotel_ou_regiao',
-'qnt_quartos',
-'tipo_quarto',
-'hospedagem_servicos',
-'hospedagem_tipo',
-'hospedagem_solicitacoes',
-'hospedagem_preco_desejado',
-'transporte_interno',
-'tipos_transfer',
-'categorias_carro',
-'itens_carro',
-'transporte_interno_solicitacoes',
-'transporte_interno_preco_desejado',
-'passeios_interesses',
-'passeios_outros',
-'passeios_preco_desejado',
-'nomes_seguro_viagem',
-'datas_nascimento_seguro_viagem',
-'nome_completo',
-'nome_preferencia',
-'email',
-'telefone'
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Prefere ser chamado de: </span>
+                        {{ $cotacao->nome_preferencia }}<br>
 
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Email: </span>
+                        {{ $cotacao->email }}<br>
 
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Telefone: </span>
+                        {{ $cotacao->telefone }}<br>
 
-                    <br> <hr>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Contato enviado em: </span>
+                        {{ $cotacao->created_at->format('d/m/Y - H:i') }}<br>
+                    </p>
+
+                    <p style="font-size:18px; font-weight:normal; margin-top:10px; text-align: left;">
+                        <span style="font-family:'Titillium Web', sans-serif;">1 - Para onde você quer ir?</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Origem: </span>
+                        {{ $cotacao->origem }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Destino: </span>
+                        {{ $cotacao->destino }}<br><br>
+
+                        <span style="font-family:'Titillium Web', sans-serif;">2 - Quando você quer ir?</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Data de ida: </span>
+                        {{ $cotacao->data_ida }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Data de volta: </span>
+                        {{ $cotacao->data_volta }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">As datas são flexíveis: </span>
+                        {{ $cotacao->datas_flexiveis }}<br><br>
+
+                        <span style="font-family:'Titillium Web', sans-serif;">3 - Com quem você quer ir?</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Número de adultos: </span>
+                        {{ $cotacao->qnt_adultos }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Número de crianças: </span>
+                        {{ $cotacao->qnt_criancas }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Número de bebês: </span>
+                        {{ $cotacao->qnt_bebes }}<br><br>
+
+                        <span style="font-family:'Titillium Web', sans-serif;">4 - Transporte Aéreo</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Horário de ida: </span>
+                        {{ $cotacao->periodo_voo_ida }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Horário de volta: </span>
+                        {{ $cotacao->periodo_voo_volta }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Aeroporto de origem: </span>
+                        {{ $cotacao->aeroporto_origem }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Aeroporto de retorno: </span>
+                        {{ $cotacao->aeroporto_retorno }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Companhias aéreas de preferência: </span>
+                        {{ $cotacao->companias_aereas_preferenciais }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Número de paradas: </span>
+                        {{ $cotacao->numero_paradas }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Tempo de vôo: </span>
+                        {{ $cotacao->tempo_voo }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Preço desejado: </span>
+                        {{ $cotacao->aereo_preco_desejado }}<br><br>
+
+                        <span style="font-family:'Titillium Web', sans-serif;">5 - Hospedagem</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Nome do hotel ou região de preferência: </span>
+                        {{ $cotacao->hotel_ou_regiao }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Quantidade de quartos: </span>
+                        {{ $cotacao->qnt_quartos }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Tipo de quarto: </span>
+                        {{ $cotacao->tipo_quarto }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Serviços incluídos desejados: </span>
+                        {{ $cotacao->hospedagem_servicos }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Tipo de hospedagem: </span>
+                        {{ $cotacao->hospedagem_tipo }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Solicitações especiais: </span>
+                        {{ $cotacao->hospedagem_solicitacoes }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Preço desejado: </span>
+                        {{ $cotacao->hospedagem_preco_desejado }}<br><br>
+
+                        <span style="font-family:'Titillium Web', sans-serif;">6 - Transporte Interno</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif;">Transfer Para o Hotel</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Transporte interno: </span>
+                        {{ $cotacao->transporte_interno }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Tipos de transfer: </span>
+                        {{ $cotacao->tipos_transfer }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif;">Aluguel de Carro</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Categoria: </span>
+                        {{ $cotacao->categorias_carro }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Itens desejados: </span>
+                        {{ $cotacao->itens_carro }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Solicitações especiais: </span>
+                        {{ $cotacao->transporte_interno_solicitacoes }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Preço desejado: </span>
+                        {{ $cotacao->transporte_interno_preco_desejado }}<br><br>
+
+                        <span style="font-family:'Titillium Web', sans-serif;">7 - Passeios e Experiências</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Interesses: </span>
+                        {{ $cotacao->passeios_interesses }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Outros passeios desejados: </span>
+                        {{ $cotacao->passeios_outros }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Preço desejado: </span>
+                        {{ $cotacao->passeios_preco_desejado }}<br><br>
+
+                        <span style="font-family:'Titillium Web', sans-serif;">8 - Seguro Viagem</span><br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Nomes dos segurados: </span>
+                        {{ $cotacao->nomes_seguro_viagem }}<br>
+                        <span style="font-family:'Titillium Web', sans-serif; font-weight:bold;">Data de nascimento dos segurados: </span>
+                        {{ $cotacao->datas_nascimento_seguro_viagem }}<br>
+                    </p>
+
+                    <br><hr>
                     <p style="font-size:12px; font-family:'Titillium Web', sans-serif; font-weight:bold; margin-top:0px;">
                         * mensagem automática gerada a partir do formulário de contato de agentes*
                     </p>
