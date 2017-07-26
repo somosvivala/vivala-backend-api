9f148c569ec75401236e449693a7383eac45c1d8
Renato Gomes <renatoo.agomes@gmail.com>
2017-07-26 02:00:58 -0300

    Commit com novo Mail PedidoCotacaoPacote

---
+++ app/Mail/PedidoCotacaoPacote.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\CotacaoPacote;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PedidoCotacaoPacote extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * cotacao - Instancia de CotacaoPacote
+     *
+     * @var App\Models\CotacaoPacote
+     */
+    public $cotacao;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(CotacaoPacote $cotacao)
+    {
+        $this->contatoGeral = $cotacao;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to(env('EMAIL_DESTINO_COTACAO_PACOTE'))
+            ->from('contato@vivala.com.br')
+            ->subject('[VIVALÁ] Novo Pedido de cotação de Pacote!')
+            ->view('emails.cotacao-pacote');
+    }
+}
