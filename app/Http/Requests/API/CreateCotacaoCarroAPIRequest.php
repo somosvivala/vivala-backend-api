beae743aa85b15e6d50bf4f70a7366a3b4ce4217
Renato Gomes <renatoo.agomes@gmail.com>
2017-08-04 04:01:17 -0300

    commit com ajuste na resposta da API

--- app/Http/Requests/API/CreateCotacaoCarroAPIRequest.php
+++ app/Http/Requests/API/CreateCotacaoCarroAPIRequest.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Requests\API;
 
+use App\Http\Requests\API\BaseAPIRequest;
 use App\Models\CotacaoCarro;
-use InfyOm\Generator\Request\APIRequest;
 
-class CreateCotacaoCarroAPIRequest extends APIRequest
+class CreateCotacaoCarroAPIRequest extends BaseAPIRequest
 {
     /**
      * Determine if the user is authorized to make this request.
