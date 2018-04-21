d0f936c98c74c559890a3a08a698938d74d10052
Renato Gomes <renatoo.agomes@gmail.com>
2018-04-21 02:09:00 -0300

    traduzindo labels

--- resources/views/cotacao_aereos/fields.blade.php
+++ resources/views/cotacao_aereos/fields.blade.php
@@ -127,6 +127,6 @@
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('cotacaoAereos.index') !!}" class="btn btn-default">Cancel</a>
+    {!! Form::submit('Salvar', ['class' => 'btn btn-primary']) !!}
+    <a href="{!! route('cotacaoAereos.index') !!}" class="btn btn-default">Cancelar</a>
 </div>
--- resources/views/cotacao_carros/fields.blade.php
+++ resources/views/cotacao_carros/fields.blade.php
@@ -72,6 +72,6 @@
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('cotacaoCarros.index') !!}" class="btn btn-default">Cancel</a>
+    {!! Form::submit('Salvar', ['class' => 'btn btn-primary']) !!}
+    <a href="{!! route('cotacaoCarros.index') !!}" class="btn btn-default">Cancelar</a>
 </div>
