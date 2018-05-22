37cb7df3f91dd8655f277de39bb61d54ac9b2175
Renato Gomes <renatoo.agomes@gmail.com>
2018-05-22 00:43:33 -0300

    fix typo / versao do laravel-mix, aparentemente tudo ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,14 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js([        
-        'resources/assets/js/app.js'
-        'resources/assets/js/tesseract.js',
+        'resources/assets/js/app.js',
+        'resources/assets/js/tesseract.js'
     ], 'public/js')
 
    .styles('resources/assets/css/dropzone.css', 'public/css/dropzone.css')
    .styles('node_modules/sweetalert2/dist/sweetalert2.css', 'public/css/sweetalert2.css')
-   .styles('node_modules/cropper/dist/cropper.css', 'public/css/cropper.css')
+   .styles('node_modules/cropperjs/dist/cropper.css', 'public/css/cropper.css')
+   .js('node_modules/cropperjs/dist/cropper.min.js', 'public/js/cropper.min.js')
 
    .sass( 'resources/assets/sass/app.scss', 'public/css/')
    .sass( 'resources/assets/sass/tesseract.scss', 'public/css/');
