0209ccefed239abeb05d6e66a8cea6ed4f46171a
Renato Gomes <renatoo.agomes@gmail.com>
2018-05-26 06:53:46 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ExpedicaoController.php
+++ app/Http/Controllers/ExpedicaoController.php
@@ -198,6 +198,7 @@ class ExpedicaoController extends AppBaseController
             ];
         } else {
             Flash::error('Erro no upload da foto!');
+
             return redirect()->back();
         }
     }
