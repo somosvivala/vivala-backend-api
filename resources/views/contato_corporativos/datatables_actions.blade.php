826dc16c01945da0227af4bc0030a5a5c8ce96c6
Renato Gomes <renatoo.agomes@gmail.com>
2018-06-05 22:00:20 -0300

    Fix nome da rota de contato corporativo

--- resources/views/contato_corporativos/datatables_actions.blade.php
+++ resources/views/contato_corporativos/datatables_actions.blade.php
@@ -1,6 +1,6 @@
-{!! Form::open(['route' => ['contatoCorporativos.destroy', $id], 'method' => 'delete']) !!}
+{!! Form::open(['route' => ['contatoCorporativo.destroy', $id], 'method' => 'delete']) !!}
 <div class='btn-group'>
-    <a href="{{ route('contatoCorporativos.show', $id) }}" class='btn btn-default btn-xs'>
+    <a href="{{ route('contatoCorporativo.show', $id) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-eye-open"></i>
     </a>
     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', [
